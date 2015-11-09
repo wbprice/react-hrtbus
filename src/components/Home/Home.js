bf94efdb880814f83357aeccaf58c17061550d18
Blaine Price <1wbprice@gmail.com>
2015-11-09 14:18:19 -0500

    Add superagent dependency and rough out Location module.  Place in home route.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Radium from 'radium';
+import Location from '../Location/Location'
 
 class Home extends React.Component {
   render() {
     return (
       <section>
         <pre>Home</pre>
+        <Location />
       </section>
     )
   }
---
+++ src/components/Location/Location.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import radium from 'radium';
+
+class Location extends React.Component {
+  render() {
+    return (
+      <pre>Location</pre>
+    );
+  }
+}
+
+export default Location
