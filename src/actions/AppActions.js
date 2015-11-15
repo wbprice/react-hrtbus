53961d786d976adeef5a3db2eac25a2ae9c9424b
Mark Carter <mark4carter@gmail.com>
2015-11-15 17:26:05 -0500

    notes

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -3,7 +3,9 @@ import AppConstants from '../constants/AppConstants'
 
 var AppActions = {
   pullStopData(){
-    
+
+      //Instead of randomJSON, we can pull the API from here and
+      //put the data inside here
       var randomJson = Math.random() * 1000000;
 
   		AppDispatcher.handlePullStopData({
--- src/stores/StopStore.js
+++ src/stores/StopStore.js
@@ -5,6 +5,7 @@ import stops from './mocks/stops'
 
 const CHANGE_EVENT = 'change'
 
+//remove this
 var jsonData = 0;
 
 class StopStore extends EventEmitter {
@@ -36,6 +37,13 @@ const stopStore = new StopStore()
 
 AppDispatcher.register(function(action) {
 
+
+  //Instead of just modifying the jsonData variable,
+  //it will modify the stops variable and automatically
+  //go through the changelistener and rerender the DOM
+  // so...  
+  // jsonData = action.action.stopData  would be (line 48)
+  // stops = action.action.stopData
   if (action.action.actionType == AppConstants.PULL_DATA) {
     jsonData = action.action.stopData;
     console.log('New Json = ' + jsonData);
